d6c310a17aeeddf188b2c966db8e252cdddf8384
jugurtamahious <jugurtamahious@gmail.com>
2024-01-30 22:21:30 +0100

    changes

--- db-init/init.sql
+++ db-init/init.sql
@@ -53,12 +53,11 @@ CREATE TABLE IF NOT EXISTS carte (
     price FLOAT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS produits (
+CREATE TABLE IF NOT EXISTS product (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    produit VARCHAR(255) NOT NULL,
+    product VARCHAR(255) NOT NULL,
     price FLOAT NOT NULL,
-    menu_id INT NOT NULL ,
+    carte_id INT NOT NULL ,
     category_id INT NOT NULL,
     
 );
