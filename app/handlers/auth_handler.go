e9f8efd257f60fde16e3d232cab45143c853c7a1
jubs_kan3ki <aitaddajuba93@gmail.com>
2024-01-31 21:12:22 +0100

    fix: debug issues in the auth module

--- app/handlers/auth_handler.go
+++ app/handlers/auth_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"foodcourt/app/api/request"
 	"foodcourt/app/api/response"
 	"foodcourt/app/auth"
 	"foodcourt/app/model"
@@ -11,73 +12,54 @@ import (
 )
 
 func RegisterHandler(c fiber.Ctx, userStore *stores.Store) error {
-	var user model.UserItem
-	//mettre structure request
-	err := json.Unmarshal(c.Body(), "la tu mets le model de la request")
-	if err != nil {
-		return c.JSON(fiber.Map{
-			"data": response.ErrorResponse(err.Error()),
-		})
+	var req request.RegisterRequest
+	if err := json.Unmarshal(c.Body(), &req); err != nil {
+		return c.JSON(fiber.Map{"data": response.ErrorResponse(err.Error())})
 	}
 
-	hashedPassword, err := auth.HashPassword(user.Password)
+	hashedPassword, err := auth.HashPassword(req.Password)
 	if err != nil {
-		return c.JSON(fiber.Map{
-			"data": response.ErrorResponse("Error while hashing password"),
-		})
+		return c.JSON(fiber.Map{"data": response.ErrorResponse("Error while hashing password")})
 	}
-	user.Password = hashedPassword
+
+	user := model.UserItem{
+		Username: req.Username,
+		Email:    req.Email,
+		Password: hashedPassword,
+	}
 
 	success, err := userStore.AddUser(user)
 	if !success || err != nil {
-		return c.JSON(fiber.Map{
-			"data": response.ErrorResponse(err.Error()),
-		})
+		return c.JSON(fiber.Map{"data": response.ErrorResponse(err.Error())})
 	}
 
 	token, err := auth.GenerateJWT(user)
 	if err != nil {
-		return c.JSON(fiber.Map{
-			"data": response.ErrorResponse("cannot generate token"),
-		})
+		return c.JSON(fiber.Map{"data": response.ErrorResponse("cannot generate token")})
 	}
 
-	return c.JSON(fiber.Map{
-		"data": response.SuccessResponse(fiber.Map{"token": token}),
-	})
+	return c.JSON(fiber.Map{"data": response.SuccessResponse(fiber.Map{"token": token})})
 }
 
 func LoginHandler(c fiber.Ctx, userStore *stores.Store) error {
-	var credentials model.UserItem
-	//metttre la structure de la requête
-	err := json.Unmarshal(c.Body(), "la tu mets la struct de la request")
-	if err != nil {
-		return c.JSON(fiber.Map{
-			"data": response.ErrorResponse(err.Error()),
-		})
+	var req request.LoginRequest
+	if err := json.Unmarshal(c.Body(), &req); err != nil {
+		return c.JSON(fiber.Map{"data": response.ErrorResponse(err.Error())})
 	}
 
-	user, err := userStore.GetOneUserByUsername(credentials.Username)
+	user, err := userStore.GetOneUserByUsername(req.Email)
 	if err != nil {
-		return c.JSON(fiber.Map{
-			"data": response.ErrorResponse("Authentication failed"),
-		})
+		return c.JSON(fiber.Map{"data": response.ErrorResponse("Authentication failed")})
 	}
 
-	if !auth.CheckPasswordHash(credentials.Password, user.Password) {
-		return c.JSON(fiber.Map{
-			"data": response.ErrorResponse("Authentication failed"),
-		})
+	if !auth.CheckPasswordHash(req.Password, user.Password) {
+		return c.JSON(fiber.Map{"data": response.ErrorResponse("Authentication failed")})
 	}
 
 	token, err := auth.GenerateJWT(user)
 	if err != nil {
-		return c.JSON(fiber.Map{
-			"data": response.ErrorResponse("Error while generating token"),
-		})
+		return c.JSON(fiber.Map{"data": response.ErrorResponse("Error while generating token")})
 	}
 
-	return c.JSON(fiber.Map{
-		"data": response.SuccessResponse(fiber.Map{"message": "login successful", "token": token}),
-	})
+	return c.JSON(fiber.Map{"data": response.SuccessResponse(fiber.Map{"message": "login successful", "token": token})})
 }
