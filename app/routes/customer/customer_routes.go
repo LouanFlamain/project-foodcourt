b00e516b4845df8d9e74f82a6bf6dc7b3fbcd296
LouanFlamain <flamainlouan@gmail.com>
2024-02-08 17:17:46 +0100

    move routes

--- app/routes/customer/customer_routes.go
+++ app/routes/customer/customer_routes.go
@@ -1,8 +1,12 @@
 package customer_routes
 
 import (
+	"encoding/json"
+	"foodcourt/app/api/request"
 	"foodcourt/app/handlers"
+	"foodcourt/app/model"
 	"foodcourt/app/stores"
+	"strconv"
 
 	"github.com/gofiber/fiber/v3"
 )
@@ -11,11 +15,53 @@ func SetUpCustomerRoute(route fiber.Router, myStore *stores.Store) {
 	route.Get("/", func(c fiber.Ctx) error {
 		return c.SendString("/api/customer")
 	})
+
 	route.Get("/restaurant/all", func(c fiber.Ctx) error {
 		return handlers.GetAllOpenRestaurant(c, myStore)
 	})
 
 
-	//restaurant
+	// On récupére la carte des restaurant 
+	route.Get("/carte/:id", func(c fiber.Ctx) error {
+		restaurantID, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error":   "Invalid restaurant_id",
+				"success": false,
+			})
+		}
+
+		return handlers.GetCarteByRestaurantId(c, myStore, restaurantID)
+	})
+
+	// trouver les produis d'une carte  
+	route.Get("/products/:id", func(c fiber.Ctx) error {
+		carteID, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error":   "Invalid restaurant_id",
+				"success": false,
+			})
+		}
+
+		return handlers.GetProductsByCarteId(c, myStore, carteID)
+	})
+
+	// creer une commande 
+	route.Post("/commande/create", func(c fiber.Ctx) error {
+		var body request.CreateCommande
+
+		
+		err := json.Unmarshal(c.Body(), &body)
+		if err != nil {
+			// Gérer l'erreur si le JSON est mal formé
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "cannot parse JSON",
+			})
+		}
+		return handlers.CreateCommande(c, myStore, model.CommandeItem{Date: body.Date , UserId: body.UserId ,
+			 RestaurantId: body.RestaurantId , Content: body.Content , Commentaire: body.Commentaire , State: body.State})
+
+	})
 
 }
--- app/routes/seller/seller_routes.go
+++ app/routes/seller/seller_routes.go
@@ -1,7 +1,12 @@
 package seller_routes
 
 import (
+	"encoding/json"
+	"foodcourt/app/api/request"
+	"foodcourt/app/handlers"
+	"foodcourt/app/model"
 	"foodcourt/app/stores"
+	"strconv"
 
 	"github.com/gofiber/fiber/v3"
 )
@@ -11,4 +16,113 @@ func SetUpSellerRoute(route fiber.Router, myStore *stores.Store) {
 	route.Get("/", func(c fiber.Ctx) error {
 		return c.SendString("/api/seller")
 	})
+
+	// Suprimmer la carte des restaurant 
+	route.Delete("/carte/:id", func(c fiber.Ctx) error {
+		carteID, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error":   "Invalid restaurant_id",
+				"success": false,
+			})
+		}
+
+		return handlers.DeleteRestaurantById(c, myStore, carteID)
+	})
+
+	// creation d'une carte 
+	route.Post("/carte/create", func(c fiber.Ctx) error {
+		var body request.CreateCarte
+
+		
+		err := json.Unmarshal(c.Body(), &body)
+		if err != nil {
+			// Gérer l'erreur si le JSON est mal formé
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "cannot parse JSON",
+			})
+		}
+		return handlers.CreateCarte(c, myStore, model.CarteItem{RestaurantId: body.RestaurantId , Description: body.Description , Price: body.Price})
+
+	})
+
+	// creer un produit à la carte 
+
+	route.Post("/product/create", func(c fiber.Ctx) error {
+		var body request.CreateProduct
+
+		
+		err := json.Unmarshal(c.Body(), &body)
+		if err != nil {
+			// Gérer l'erreur si le JSON est mal formé
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "cannot parse JSON",
+			})
+		}
+		return handlers.CreateProduct(c, myStore, model.ProductItem{Produit: body.Produit , Price: body.Price , CarteId: body.CarteId , CategoryId: body.CategoryId })
+
+	})
+
+	// supprimer les produis d'une carte  
+	route.Delete("/products/:id", func(c fiber.Ctx) error {
+		productID, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error":   "Invalid restaurant_id",
+				"success": false,
+			})
+		}
+
+		return handlers.DeleteRProductsById(c, myStore, productID)
+	})
+
+	// GET commande by id 
+	route.Get("/commande/restaurant/:id", func(c fiber.Ctx) error {
+		commandeID, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error":   "Invalid restaurant_id",
+				"success": false,
+			})
+		}
+
+		return handlers.GetCommandeById(c, myStore, commandeID)
+	})
+	// GET commande by restaurantId 
+	route.Get("/commande/:id", func(c fiber.Ctx) error {
+		commandeID, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error":   "Invalid restaurant_id",
+				"success": false,
+			})
+		}
+
+		return handlers.GetAllCommandeByRestaurantId(c, myStore, commandeID)
+	})
+
+	// Update commande By Id
+
+	route.Patch("/commande/update/:id", func(c fiber.Ctx) error {
+		var body request.UpdateCommande
+
+		err := json.Unmarshal(c.Body(), &body)
+		if err != nil {
+			// Gérer l'erreur si le JSON est mal formé
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "cannot parse JSON",
+			})
+		}
+		commandeID, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error":   "Invalid restaurant_id",
+				"success": false,
+			})
+		}
+		return handlers.UpdateCommande(c, myStore, body.State, commandeID)
+
+	})
+
 }
+
